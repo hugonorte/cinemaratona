7678791668923379464ad22639d67f70ab63c597
Hugo Norte <hugonorte@gmail.com>
2025-03-13 18:05:38 -0300

    Removing duplocated code

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,30 +1,11 @@
 import React, { useState } from "react";
-import { create } from "zustand";
-import { MovieState } from "../../types";
 import Title from "../../components/title";
 import BaseLayout from "../../components/layout";
 import Search from "../../components/search/input";
 import SearchResultsContainer from "../../components/search/search_results";
 import CardMovieSearch from "../../components/card/movie/search";
 import style from "./style.module.scss";
-
-const BASE_URL = "https://api.themoviedb.org/3/search/movie";
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNjNlMDdmNWY5ZmZmYmZkZDdjNWZjNDQxMGY4MzU0ZiIsIm5iZiI6MTU1MDQxMzEwMy41NDcwMDAyLCJzdWIiOiI1YzY5NmQyZjkyNTE0MTdkZjQwNDE0OTMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.kjEgvXZiIApfzW8_hhoyav3Q-C5WQqbDbadhAXxoaZs'
-  }
-};
-
-const useMovieStore = create<MovieState>((set) => ({
-  movies: [],
-  fetchMovies: async (title: string) => {
-    const response = await fetch(`${BASE_URL}?query=${title}`, options);
-    const data = await response.json();
-      set({ movies: data.results || [] });
-  },
-}));
+import { useMovieStore } from "../../store/useMovieStore";
 
 const MovieSearch = () => {
   const [query, setQuery] = useState("");

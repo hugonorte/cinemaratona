8e41e0433c56a0814e5697161861b23dabca7577
Gabriel Aguiar <gabriielmaciel18@gmail.com>
2025-03-25 19:21:02 -0300

    atualização

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,60 +1,85 @@
 import { useState } from "react";
 import BaseLayout from "../../components/layout";
-import button from "../../components/button";
-import style from './style.module.scss';
 import ButtonPrimary from "../../components/button";
-import styleDoComponente from "../../components/button/style.module.scss"
+import style from "./style.module.scss";
+import styleDoComponente from "../../components/button/style.module.scss";
 
-const login = () => {
-    const [userName, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+const Login = () => {
+  const [userName, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log("teste", userName, password);
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
+    const userData = {
+      email: userName,
+      password: password,
     };
 
-    return (
-       <BaseLayout>
-       <div className={style.container}>
+    try {
+      const response = await fetch("URL da API", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData), 
+      });
+
+      const data = await response.json(); 
+
+
+      //Incompleto, verificar com o grupo.
+      if (response.ok) {
+        alert("Login realizado com sucesso!");
+        console.log("Token recebido:", data.token);
+      } else {
+        alert(`Erro: ${data.message}`);
+      }
+    } catch (error) {
+      console.error("Erro ao enviar dados:", error);
+      alert("Erro ao conectar com o servidor");
+    }
+  };
+
+  return (
+    <BaseLayout>
+      <div className={style.container}>
         <form onSubmit={handleSubmit}>
-            <h1>Acesse</h1>
-            <div className={style.inputField}>
-            <input 
-                type="email" 
-                value={userName} 
-                onChange={(e) => setUsername(e.target.value)} 
-                placeholder="Email"
+          <h1>Acesse</h1>
+          <div className={style.inputField}>
+            <input
+              type="email"
+              value={userName}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Email"
             />
-            </div>
-            <div className={style.inputField}>
-            <input 
-                type="password" 
-                value={password} 
-                onChange={(e) => setPassword(e.target.value)} 
-                placeholder="Senha"
+          </div>
+          <div className={style.inputField}>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Senha"
             />
-            </div>
+          </div>
 
-            <div className={style.recallForget}>
-                <label>
-                    <input type="checkbox" />
-                    Lembrar de mim
-                </label>
-                <a href="#">Esqueci minha senha</a>
-            </div>
+          <div className={style.recallForget}>
+            <label>
+              <input type="checkbox" />
+              Lembrar de mim
+            </label>
+            <a href="#">Esqueci minha senha</a>
+          </div>
 
-            <ButtonPrimary className={styleDoComponente.button} type="submit" label="Entrar" />
+          <ButtonPrimary className={styleDoComponente.button} type="submit" label="Entrar" />
 
-            <div className={style.signup}>
-                <a href="#">Criar uma conta</a>
-            </div>
+          <div className={style.signup}>
+            <a href="#">Criar uma conta</a>
+          </div>
         </form>
-        </div>
-        </BaseLayout>
-    );
-
+      </div>
+    </BaseLayout>
+  );
 };
 
-export default login;
+export default Login;

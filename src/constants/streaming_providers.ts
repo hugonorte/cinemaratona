18a7b3ef084f347c2c965548d01fcb94966bb92f
Hugo Norte <hugonorte@gmail.com>
2025-03-27 19:36:31 -0300

    adding streaming logos

--- src/constants/streaming_providers.ts
+++ src/constants/streaming_providers.ts
@@ -2,7 +2,7 @@ export enum StreamingProviders {
     NETFLIX = 8,
     PRIME_VIDEO = 119,
     MAX = 1899,
-    PARAMONT = 531,
+    PARAMOUNT = 531,
     DISNEY = 337,
     APPLE = 2,
     GLOBO_PLAY = 307,

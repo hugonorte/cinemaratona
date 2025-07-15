10c7d9f006af4d60c5874d2076533036984878e1
Hugo Norte <hugonorte@gmail.com>
2025-07-14 22:04:08 -0300

    Add functionality to fetch and manage watched movies for users

---
+++ src/api/movie/watched/getAllMoviesWatched.ts
@@ -0,0 +1,40 @@
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+export async function getAllMoviesWatched(
+    userId: number,
+) {
+    const query = `
+        query {
+            watchedMoviesByUser(user_id: ${userId}) {
+                total
+                movies {
+                    id
+                    movie_id_from_api
+                    title
+                    release_date
+                    poster
+                }
+            }
+        }
+    `;
+  try {
+    const response = await fetch(`${BASE_URL}/graphql`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error fetching watched movies: ${response.statusText}`);
+    }
+    
+    const data = await response.json();
+    console.log("foo", data.data.watchedMoviesByUser)
+    return data.data.watchedMoviesByUser;
+  } catch (error) {
+    console.error('Failed to fetch watched movies:', error);
+    throw error;
+  }
+}
